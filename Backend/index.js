13755ab3ec5ef5e73bfdab69e2b2ee145a63c1d5
sanket malvi <sanketmalvi5@gmail.com>
2024-04-18 17:38:24 +0530

    Added login Signup

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import cors from "cors";
+import cors from "cors";
 
 import bookRoute from "./route/book.route.js";
-// import userRoute from "./route/user.route.js";
+import userRoute from "./route/user.route.js";
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 dotenv.config();
@@ -29,7 +29,7 @@ try {
 
 // defining routes
 app.use("/book", bookRoute);
-// app.use("/user", userRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
